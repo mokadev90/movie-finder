feaa46aa3aae03539416b9464862ab96bbf4295b
mokadev <romeramatias@gmail.com>
2024-09-10 02:26:31 -0300

    ci: add semantic release

---
+++ .releaserc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+  messageMaxLength: 0,
+};
