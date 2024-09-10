ce96b1e562a720817001ae846dc75268a45c12bc
mokadev <romeramatias@gmail.com>
2024-09-10 10:29:43 -0300

    chore: fix redux ssr

--- redux/store.ts
+++ redux/store.ts
@@ -1,13 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterSlice from './slices';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterSlice,
-  },
-});
+export const makeStore = () =>
+  configureStore({
+    reducer: {},
+  });
 
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
