f8ede2b90723a80d40ca67dab167b3b7746ae851
mokadev <romeramatias@gmail.com>
2024-09-10 21:09:50 -0300

    chore: add image domain in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
