7fa7e3aa50aae5ca6e4338bd916aeeec998d7eee
mokadev <romeramatias@gmail.com>
2024-09-10 21:13:19 -0300

    feat: update apiService

--- services/axiosInstance.ts
+++ services/axiosInstance.ts
@@ -1,12 +1,14 @@
 import axios, { AxiosInstance } from 'axios';
 
 const baseURL = process.env.API_BASE_URL;
+const accessToken = process.env.API_ACCESS_TOKEN;
 
 const apiService: AxiosInstance = axios.create({
   baseURL,
   paramsSerializer: params => JSON.stringify(params),
   headers: {
     'Content-Type': 'application/json',
+    Authorization: `Bearer ${accessToken}`,
   },
 });
 
